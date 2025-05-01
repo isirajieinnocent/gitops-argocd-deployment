f9fddcf84a77c958f02d4e4b8cd62bda926d3d20
isirajieinnocent <isirajieinnocent@gmail.com>
2025-05-02 01:12:04 +0300

    terraform update

--- terraform_configs/argocd.tf
+++ terraform_configs/argocd.tf
@@ -1,24 +1,16 @@
-# Define the Kubernetes namespace resource for ArgoCD
-resource "kubernetes_namespace" "argocd" {
-  metadata {
-    name = "argocd"
-  }
-}
-
-# Define the Helm release resource for ArgoCD
 resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  namespace  = kubernetes_namespace.argocd.metadata[0].name  # Reference the argocd namespace defined above
+  namespace  = "argocd"
   version    = "5.51.6"
+  create_namespace = true
 
   values = [
     <<-EOF
       server:
         service:
-          type: clusterIP
-          EOF
-
-    ]
+          type: LoadBalancer
+    EOF
+  ]
 }
