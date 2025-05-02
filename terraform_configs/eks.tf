152a00a901aca5362d5e81e8024560c319fb2389
isirajieinnocent <isirajieinnocent@gmail.com>
2025-05-02 03:26:24 +0300

    ok

--- terraform_configs/eks.tf
+++ terraform_configs/eks.tf
@@ -1,35 +1,37 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.16"
-
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
-  enable_irsa     = true
-  vpc_id          = var.vpc_id
-  subnet_ids      = var.subnet_ids
-
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = true
-
-  eks_managed_node_groups = {
-    spot_workers = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-
-      capacity_type  = "SPOT"
-      instance_types = ["t3.medium", "t3a.medium"]
-
-      labels = {
-        workload-type = "spot"
-      }
-
-      # Taints removed
-    }
-  }
-
-  tags = {
-    Environment = "prod"
-    Terraform   = "true"
-  }
-}
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: release-name
+spec:
+  replicas: 3
+  selector:
+    matchLabels:
+      app: release-name
+  template:
+    metadata:
+      labels:
+        app: release-name
+    spec:
+      securityContext:
+        fsGroup: 2000
+        runAsNonRoot: true
+        runAsUser: 1000
+        seccompProfile:
+          type: RuntimeDefault
+      containers:
+        - name: app
+          image: "ghcr.io/isirajieinnocent/time-printer:latest"
+          ports:
+            - containerPort: 8080
+server:
+  extraArgs:
+    - --tls-cert-file=/etc/argocd/tls/argocd-server.crt
+    - --tls-key-file=/etc/argocd/tls/argocd-server.key
+  extraVolumeMounts:
+    - name: argocd-server-tls
+      mountPath: /etc/argocd/tls
+      readOnly: true
+  extraVolumes:
+    - name: argocd-server-tls
+      secret:
+        secretName: argocd-server-tls
